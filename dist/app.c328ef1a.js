e9b74d07bbe1db0a2bb3c61bf436ded016bd9868
CarloBerni <carlo.berni@hetic.net>
2019-06-27 11:50:19 +0200

    delete jquery

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -26387,7 +26387,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49390" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50273" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
